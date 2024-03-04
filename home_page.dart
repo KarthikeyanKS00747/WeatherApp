5f7a3382e7c80e36a853f04054ed920176b90c8a
Karthikeyan KS <86113017+KarthikeyanKS00747@users.noreply.github.com>
2024-03-05 00:08:00 +0530

    Update home_page.dart

--- home_page.dart
+++ home_page.dart
@@ -1,151 +1,133 @@
-
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:weather/weather.dart';
 import 'package:weather_app/const.dart';
 
-class HomePage extends StatefulWidget
-{
+// HomePage is a StatefulWidget that displays weather information.
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
-
 }
 
-class _HomePageState extends State<HomePage>
-{
+class _HomePageState extends State<HomePage> {
+  // WeatherFactory is used to fetch weather data.
   final WeatherFactory _wf = WeatherFactory(OPEN_WEATHER_API_KEY);
-  Weather? _weather;
+  Weather? _weather; // Holds the current weather data.
 
   @override
-  void initState()
-  {
+  void initState() {
     super.initState();
-    _wf.currentWeatherByCityName("Delhi").then((w)
-    {
-      setState(()
-      {
+    // Fetch the current weather for Delhi when the widget is initialized.
+    _wf.currentWeatherByCityName("Delhi").then((w) {
+      setState(() {
         _weather = w;
-      }
-      );
-    }
-    );
-  }
-  @override
-  Widget build(BuildContext context)
-  {
-    return Scaffold
-      (
-        body : _buildUI()
-      );
+      });
+    });
   }
 
-  Widget _buildUI()
-  {
-    if (_weather == null)
-    {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: _buildUI());
+  }
+
+  // _buildUI constructs the UI for the HomePage.
+  Widget _buildUI() {
+    if (_weather == null) {
+      // Show a loading spinner if the weather data hasn't been loaded yet.
       return const Center(
-        child : CircularProgressIndicator(),
+        child: CircularProgressIndicator(),
       );
     }
+    // Once the weather data is loaded, display it.
     return SizedBox(
-      width : MediaQuery.sizeOf(context).width,
-      height : MediaQuery.sizeOf(context).height,
-      child: Column
-      (
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Column(
         mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           _locationHeader(),
-          SizedBox(height: MediaQuery.sizeOf(context).height * 0.08,),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.08),
           _dateTimeInfo(),
-          SizedBox(height: MediaQuery.sizeOf(context).height * 0.05,),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.05),
           _weatherIcon(),
-          SizedBox(height: MediaQuery.sizeOf(context).height * 0.02,),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.02),
           _currentTemp(),
-          SizedBox(
-            height: MediaQuery.sizeOf(context).height * 0.02,
-          ),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.02),
           _extraInfo(),
-      ]
-      )
+        ],
+      ),
     );
   }
-  Widget _locationHeader()
-  {
-    return Text
-    (
+
+  // _locationHeader displays the name of the location.
+  Widget _locationHeader() {
+    return Text(
       _weather?.areaName ?? "",
-      style: const TextStyle
-      (
-        fontSize : 20, 
-        fontWeight : FontWeight.w500
-      )
-  );
+      style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+    );
   }
 
-  Widget _dateTimeInfo()
-  {
+  // _dateTimeInfo displays the current date and time.
+  Widget _dateTimeInfo() {
     DateTime now = _weather!.date!;
     return Column(
       children: [
-        Text
-        (
+        Text(
           DateFormat("h:mm a").format(now),
-          style : const TextStyle(fontSize: 30)
+          style: const TextStyle(fontSize: 30),
         ),
         const SizedBox(height: 10),
-        Row 
-        (
-          
+        Row(
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(DateFormat("EEEE").format(now),
-            style: const TextStyle(fontWeight: FontWeight.w900)),
+                style: const TextStyle(fontWeight: FontWeight.w900)),
             Text(" "),
             Text(DateFormat("d.m.y").format(now),
-            style: const TextStyle(fontWeight: FontWeight.w900)),
-          ]
-        )
+                style: const TextStyle(fontWeight: FontWeight.w900)),
+          ],
+        ),
       ],
     );
   }
-  Widget _weatherIcon()
-  {
+
+  // _weatherIcon displays the current weather icon and description.
+  Widget _weatherIcon() {
     return Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Container
-        (
-          height: MediaQuery.sizeOf(context).height * 0.40,
-          decoration: 
-          BoxDecoration
-          (
-            image: DecorationImage
-            ( 
-              image : NetworkImage("http://openweathermap.org/img/wn/${_weather?.weatherIcon}@4x.png")
-            
-            )
+        Container(
+          height: MediaQuery.of(context).size.height * 0.40,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                  "http://openweathermap.org/img/wn/${_weather?.weatherIcon}@4x.png"),
+            ),
           ),
         ),
-      Text(_weather?.weatherDescription ?? "")
+        Text(_weather?.weatherDescription ?? ""),
       ],
     );
   }
-  Widget _currentTemp()
-  {
-    return Text("${_weather?.temperature?.celsius?.toStringAsFixed(0)}° C",);
+
+  // _currentTemp displays the current temperature.
+  Widget _currentTemp() {
+    return Text("${_weather?.temperature?.celsius?.toStringAsFixed(0)}° C");
   }
-   Widget _extraInfo() {
+
+  // _extraInfo displays additional weather information.
+  Widget _extraInfo() {
     return Container(
-      height: MediaQuery.sizeOf(context).height * 0.15,
-      width: MediaQuery.sizeOf(context).width * 0.80,
+      height: MediaQuery.of(context).size.height * 0.15,
+      width: MediaQuery.of(context).size.width * 0.80,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(
           20,
@@ -166,17 +148,15 @@ class _HomePageState extends State<HomePage>
               Text(
                 "Max: ${_weather?.tempMax?.celsius?.toStringAsFixed(0)}° C",
                 style: const TextStyle(
-            
                   fontSize: 15,
                 ),
               ),
               Text(
                 "Min: ${_weather?.tempMin?.celsius?.toStringAsFixed(0)}° C",
                 style: const TextStyle(
-                  
                   fontSize: 15,
                 ),
-              )
+              ),
             ],
           ),
           Row(
@@ -187,19 +167,17 @@ class _HomePageState extends State<HomePage>
               Text(
                 "Wind: ${_weather?.windSpeed?.toStringAsFixed(0)}m/s",
                 style: const TextStyle(
-                  
                   fontSize: 15,
                 ),
               ),
               Text(
                 "Humidity: ${_weather?.humidity?.toStringAsFixed(0)}%",
                 style: const TextStyle(
-                  
                   fontSize: 15,
                 ),
-              )
+              ),
             ],
-          )
+          ),
         ],
       ),
     );
